47d69790825ea4ee77b00ff6ac2bf6deeda3479c
maracax <alaneraerts@live.be>
2020-12-19 01:34:07 +0100

    rendu conditionnel + loader

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ()=>{
+    return (
+        <div style={ { minHeight:"100vh" } } className={"d-flex flex-row justify-content-center align-items-center w-100"}>
+            <img src="https://assets.materialup.com/uploads/c6980863-d7bf-41a6-9272-b6e2465b60c2/attachment.gif" alt="loading"/>
+        </div>
+    )
+}
